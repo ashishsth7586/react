ceb60a5ef548c4752fea0331ffdce86ab2f47718
ashishsth7586 <ashishsth7586@gmail.com>
2019-07-20 00:12:34 +0545

    Key removed

--- modernReact/pics/src/api/unsplash.js
+++ modernReact/pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID 29efe52c238ff4477c929fdd8991ec92a83172b97d0a0ca6c144bcbf6566abcd '
+        Authorization: 'Client-ID Key '
     }
 })
