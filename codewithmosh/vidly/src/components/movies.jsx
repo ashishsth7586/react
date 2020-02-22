6268fe53710be7fec766a57a6ebe52a65d503b64
ashishsth7586 <ashishsth7586@gmail.com>
2020-02-22 23:19:37 +0545

    vidly first commit

---
+++ codewithmosh/vidly/src/components/movies.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = { movies: getMovies() };
+
+  handleDelete = movie => {};
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map(movie => (
+            <tr>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button className="btn btn-danger btn-sm">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Movies;
---
+++ codewithmosh/vidly/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
