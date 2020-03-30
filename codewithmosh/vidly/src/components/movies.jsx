62336277bee05c73bb8d088bb02473c1b969e58c
ashishsth7586 <ashishsth7586@gmail.com>
2020-03-30 21:16:14 +0545

    composing components completed

--- codewithmosh/vidly/src/components/movies.jsx
+++ codewithmosh/vidly/src/components/movies.jsx
@@ -1,32 +1,15 @@
 import React, { Component } from "react";
+import Like from "./common/like";
 import { getMovies } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
-import Pagination from "./common/pagination";
-import { paginate } from "../utils/paginate";
-import ListGroup from "./common/listGroup";
-import MoviesTable from "./moviesTable";
-import _ from "lodash";
 
 class Movies extends Component {
   state = {
-    movies: [],
-    pageSize: 4,
-    currentPage: 1,
-    genres: [],
-    sortColumn: { path: "title", order: "asc" }
+    movies: getMovies()
   };
 
-  componentDidMount() {
-    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
-    this.setState({ movies: getMovies(), genres: genres });
-  }
-
   handleDelete = movie => {
-    // Get all the movies except the movies passed as argument
     const movies = this.state.movies.filter(m => m._id !== movie._id);
-    this.setState({
-      movies
-    });
+    this.setState({ movies });
   };
 
   handleLike = movie => {
@@ -34,77 +17,54 @@ class Movies extends Component {
     const index = movies.indexOf(movie);
     movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
-    this.setState({
-      movies
-    });
-  };
-
-  handlePageChange = page => {
-    this.setState({ currentPage: page });
-  };
-
-  handleGenreSelect = genre => {
-    this.setState({ selectedGenre: genre, currentPage: 1 });
-  };
-
-  handleSort = path => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-    this.setState({ sortColumn });
+    this.setState({ movies });
   };
 
   render() {
     const { length: count } = this.state.movies;
-    const {
-      pageSize,
-      currentPage,
-      movies: allMovies,
-      selectedGenre,
-      sortColumn
-    } = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
-    const filtered =
-      selectedGenre && selectedGenre._id
-        ? allMovies.filter(m => m.genre._id === selectedGenre._id)
-        : allMovies;
-
-    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
-    const movies = paginate(sorted, currentPage, pageSize);
-
     return (
-      <div className="row">
-        <div className="col-3">
-          <ListGroup
-            items={this.state.genres}
-            selectedItem={this.state.selectedGenre}
-            onItemSelect={this.handleGenreSelect}
-          />
-        </div>
-        <div className="col">
-          <p>Showing {filtered.length} movies in the database.</p>
-
-          <MoviesTable
-            movies={movies}
-            onDelete={this.handleDelete}
-            onLike={this.handleLike}
-            onSort={this.handleSort}
-          />
-
-          <Pagination
-            itemsCount={filtered.length}
-            pageSize={pageSize}
-            onPageChange={this.handlePageChange}
-            currentPage={currentPage}
-          />
-        </div>
-      </div>
+      <React.Fragment>
+        <p>Showing {count} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
--- codewithmosh/vidly/src/index.js
+++ codewithmosh/vidly/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+
 ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
